248b18ac0229b0acb40d89bf08a8b06a4b8b8c67
gedoujou <gedoujou55@gmail.com>
2026-06-07 18:36:33 +0900

    some changes

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(`
-    <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-      <meta charset="UTF-8">
-      <title>マイサイト</title>
-    </head>
-    <body>
-      <h1>こんにちは！</h1>
-      <p>Vercelで動いてます。</p>
-    </body>
-    </html>
-  `);
+  const filePath = path.join(__dirname, 'index.html');
+  
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('エラーが発生しました');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
 });
 
 server.listen(process.env.PORT || 3000);
