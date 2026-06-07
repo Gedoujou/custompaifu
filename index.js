33db7c136cf71b4ff26f6a0f853631c08f535066
gedoujou <gedoujou55@gmail.com>
2026-06-07 19:40:53 +0900

    some changes

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 const https = require('https');
-const path = require('path');
-
-const mimeTypes = {
-  '.html': 'text/html',
-  '.css': 'text/css',
-  '.js': 'application/javascript',
-};
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url !== '/') {
-    const ext = path.extname(req.url);
-    const filePath = path.join(__dirname, req.url);
-
-    if (mimeTypes[ext] && fs.existsSync(filePath)) {
-      res.writeHead(200, { 'Content-Type': mimeTypes[ext] });
-      fs.createReadStream(filePath).pipe(res);
-      return;
-    }
-  }
-
   if (req.method === 'GET') {
     fs.readFile('index.html', 'utf8', (err, data) => {
       res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -36,11 +18,7 @@ const server = http.createServer((req, res) => {
       const { logId } = JSON.parse(body);
       const url = `https://tenhou.net/0/log/?${logId}`;
 
-      const options = {
-        headers: { 'User-Agent': 'Mozilla/5.0' }
-      };
-
-      https.get(url, options, (fileRes) => {
+      https.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' } }, (fileRes) => {
         let data = '';
         fileRes.setEncoding('utf8');
         fileRes.on('data', chunk => data += chunk);
