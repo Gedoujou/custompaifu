0449534ea41b33da0294fd06fe8028ed8641fa3e
gedoujou <gedoujou55@gmail.com>
2026-06-07 20:47:39 +0900

    index.js changes

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css':  'text/css',
+  '.js':   'text/javascript',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+};
+
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
-  
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  let reqPath = req.url === '/' ? '/index.html' : req.url;
+  const filePath = path.join(__dirname, reqPath);
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.writeHead(500);
-      res.end('エラーが発生しました');
+      res.writeHead(404);
+      res.end('Not Found');
       return;
     }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { 'Content-Type': contentType });
     res.end(data);
   });
 });
