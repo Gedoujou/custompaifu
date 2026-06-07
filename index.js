00921b78a678b95b82237f599683c5b8841e1d5c
gedoujou <gedoujou55@gmail.com>
2026-06-07 19:53:37 +0900

    some changes

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
 const http = require('http');
 const fs = require('fs');
-const https = require('https');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    fs.readFile('index.html', 'utf8', (err, data) => {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    });
-    return;
-  }
-
-  if (req.method === 'POST' && req.url === '/fetch-log') {
-    let body = '';
-    req.on('data', chunk => body += chunk);
-    req.on('end', () => {
-      const { logId } = JSON.parse(body);
-      const url = `https://tenhou.net/0/log/?${logId}`;
-
-      https.get(url, { headers: { 'User-Agent': 'Mozilla/5.0' } }, (fileRes) => {
-        let data = '';
-        fileRes.setEncoding('utf8');
-        fileRes.on('data', chunk => data += chunk);
-        fileRes.on('end', () => {
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end(data);
-        });
-      }).on('error', (err) => {
-        res.writeHead(500);
-        res.end('取得失敗: ' + err.message);
-      });
-    });
-    return;
-  }
+  const filePath = path.join(__dirname, 'index.html');
 });
-
-server.listen(process.env.PORT || 3000);
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('エラーが発生しました');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
